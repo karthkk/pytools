8e43c3501c57a17c77f98852afb99b149ff32963
karthik <karthkk@gmail.com>
2016-12-28 16:55:48 -0800

    added tf composable layers from https://github.com/ethereon/caffe-tensorflow/blob/master/kaffe/tensorflow/network.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pytools',
+      version='1.0',
+      description='Useful python tools',
+      author="Karthik",
+      author_email='karthikkumara@outlook.com',
+      url='https://github.com/karthkk/pytools',
+      packages=['tftools'],
+     )
